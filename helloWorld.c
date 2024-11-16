352967feb39198c0e167bf473e9376c2a0622045
Lucas Silva <139870890+j0jds@users.noreply.github.com>
2024-11-15 21:11:47 -0300

    Create helloWorld.c
    First task (months ago)

---
+++ helloWorld.c
@@ -0,0 +1,23 @@
+/* 
+
+Task
+
+This challenge requires you to print "Hello, World!" on a single line, and then print the already provided input string to stdout.
+
+Sample Output 0
+
+Hello, World!
+Welcome to C programming.
+
+*/
+
+// "Hello World!" in C
+
+#include <stdio.h>
+
+int main () {
+    printf("Hello, World!\n");
+    printf("Welcome to C programming.");
+    
+    return 0;
+}
